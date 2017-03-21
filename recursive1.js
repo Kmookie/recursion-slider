13fb3d6450a5f97ae096cc7dc2cd9f29ab05a98b
MorganD <morgan_durocher@hotmail.com>
2017-03-21 05:36:31 +0000

    First Draft

---
+++ recursive1.js
@@ -0,0 +1,216 @@
+// document.title = "MorgansRecursiveCrazyNess";
+// document.body.style.width = '31000px';
+
+
+function fibHelper(n) {
+	var value;
+	var div = document.createElement('div');
+
+	  div.setAttribute("class", "fib");
+
+	n = parseInt(n);
+
+	// leaf nodes aka. base case
+	if (n < 2) {
+		if (n === 0) {
+			value = 0;
+		}
+		else if (n === 1) {
+			value = 1;
+		}
+		var p = document.createElement('p');
+		p.textContent = 'Fib(' + n + ') = ' + value;
+		div.appendChild(p)
+	}
+	else {
+		var left = fibHelper(n - 1);
+		var clas = left.html.getAttribute('class');
+		left.html.setAttribute('class', clas + ' fib-left');
+
+		var right = fibHelper(n - 2);
+		clas = right.html.getAttribute('class');
+		right.html.setAttribute('class', clas + ' fib-right');
+
+		value = left.value + right.value;
+		var p = document.createElement('p');
+		p.textContent = 'Fib(' + n + ') = ' + value;
+		div.appendChild(p);
+
+		div.appendChild(left.html);
+		div.appendChild(right.html);
+	}
+
+	return { 'value': value, 'html': div };
+}
+
+var fib = function (n, node) {
+	var fibTree = node.querySelector('div.fib');
+	if (fibTree) {
+		node.removeChild(fibTree);
+	}
+
+	var tree = fibHelper(n);
+	tree.html.style.display = 'inline-block';
+
+	node.appendChild(tree.html);
+}
+
+var fibButton = function(me) {
+	var form = me.parentNode;
+	var slider = form.querySelector('input');
+	var value = slider.value;
+	fib(value, form.parentNode);
+}
+
+var fibSlider = function(me) {
+	var form = me.parentNode;
+	var button = form.querySelector('button');
+	button.textContent = 'Fib(' + me.value + ')';
+}
+
+////////////////////////////////////////////////////////////////////////////////////
+function pellHelper(n) {
+	var value;
+	var div = document.createElement('div');
+
+	div.setAttribute("class", "pell");
+
+	n = parseInt(n);
+
+	// leaf nodes aka. base case
+	if (n < 2) {
+		if (n === 0) {
+			value = 0;
+		}
+		else if (n === 1) {
+			value = 1;
+		}
+		var p = document.createElement('p');
+		p.textContent = 'Pell(' + n + ') = ' + value;
+		div.appendChild(p)
+	}
+	else {
+		var left = pellHelper(n - 1);
+		var clas = left.html.getAttribute('class');
+		left.html.setAttribute('class', clas + ' fib-left');
+
+		var right = pellHelper(n - 2);
+		clas = right.html.getAttribute('class');
+		right.html.setAttribute('class', clas + ' fib-right');
+
+		value = left.value + right.value;
+		var p = document.createElement('p');
+		p.textContent = 'Pell(' + n + ') = ' + value;
+		div.appendChild(p);
+
+
+
+		div.appendChild(left.html);
+		div.appendChild(right.html);
+	}
+
+	return { 'value': value, 'html': div };
+}
+
+var pell = function (n, node) {
+	var pellTree = node.querySelector('div.pell');
+	if (pellTree) {
+		node.removeChild(pellTree);
+	}
+
+	var tree = pellHelper(n);
+	tree.html.style.display = 'inline-block';
+
+	node.appendChild(tree.html);
+}
+
+var pellButton = function(me) {
+	var form = me.parentNode;
+	var slider = form.querySelector('input');
+	var value = slider.value;
+	pell(value, form.parentNode);
+}
+
+var pellSlider = function(me) {
+	var form = me.parentNode;
+	var button = form.querySelector('button');
+	button.textContent = 'Pell(' + me.value + ')';
+}
+
+///////////////////////////////////////////////////////////////////////////////////
+function tribHelper(n) {
+	var value;
+	var div = document.createElement('div');
+
+	div.setAttribute("class", "trib");
+
+	n = parseInt(n);
+
+	// leaf nodes aka. base case
+	if (n <= 2) {
+		if (n === 0) {
+			value = 0;
+		}
+		else if (n === 1) {
+			value = 0;
+		}
+		else if (n===2){
+			value = 1;
+		}
+		var p = document.createElement('p');
+		p.textContent = 'Trib(' + n + ') = ' + value;
+		div.appendChild(p)
+	}
+	else {
+		var left = tribHelper(n - 1);
+		var clas = left.html.getAttribute('class');
+		left.html.setAttribute('class', clas + ' fib-left');
+
+		var center = tribHelper(n - 2);
+		clas = center.html.getAttribute('class');
+		center.html.setAttribute('class', clas + ' fib-center');
+
+		var right = tribHelper(n - 3);
+		clas = right.html.getAttribute('class');
+		right.html.setAttribute('class', clas + ' fib-right');
+
+
+		value = left.value + center.value +right.value;
+		var p = document.createElement('p');
+		p.textContent = 'Trib(' + n + ') = ' + value;
+		div.appendChild(p);
+
+
+
+		div.appendChild(left.html);
+		div.appendChild(center.html);
+		div.appendChild(right.html);
+	}
+
+	return { 'value': value, 'html': div };
+}
+
+var trib = function (n, node) {
+	var tribTree = node.querySelector('div.trib');
+	if (tribTree) {
+		node.removeChild(tribTree);
+	}
+
+	var tree = tribHelper(n);
+	tree.html.style.display = 'inline-block';
+
+	node.appendChild(tree.html);
+}
+
+var tribButton = function(me) {
+	var form = me.parentNode;
+	var slider = form.querySelector('input');
+	var value = slider.value;
+	trib(value, form.parentNode);
+}
+
+var tribSlider = function(me) {
+	var form = me.parentNode;
+	var button = form.querySelector('button');
+	button.textContent = 'Trib(' + me.value + ')';
+}
